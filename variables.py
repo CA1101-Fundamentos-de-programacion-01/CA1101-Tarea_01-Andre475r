b88eb94578af0e2929ba1bfd2f45f99120147947
Andre475r <98907301+Andre475r@users.noreply.github.com>
2022-02-04 20:11:01 -0600

    Update variables.py

--- variables.py
+++ variables.py
@@ -4,7 +4,7 @@ monedas_robadas = 10
 
 monedas_magicas = 3
 
-resultado =monedas_encontradas + monedas_robadas * 365 -monedas_magicas * 522
+resultado =monedas_encontradas + monedas_robadas * 365 -monedas_magicas *52
 
 
 
